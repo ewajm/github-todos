9855faf5294c57555e97ced1609886ba0f0d48b2
Ewa Manek <manekej@gmail.com>
2016-11-27 14:24:42 -0800

    Implement todo adding (not currently persistent)

--- app/src/main/java/com/epicodus/githubtodos/TodosActivity.java
+++ app/src/main/java/com/epicodus/githubtodos/TodosActivity.java
@@ -1,13 +1,19 @@
 package com.epicodus.githubtodos;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -22,6 +28,12 @@ public class TodosActivity extends AppCompatActivity {
     TextView mProjectNameView;
     @Bind(R.id.todoListView)
     ListView mTodoListView;
+    @Bind(R.id.addTodoInput)
+    EditText mAddTodoInput;
+    @Bind(R.id.addTodoButton)
+    Button mAddTodoButton;
+    private ArrayList<String> mThisTodoArray;
+    private ArrayAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +48,26 @@ public class TodosActivity extends AppCompatActivity {
         Typeface sciFont = Typeface.createFromAsset(getAssets(), "fonts/SciFly-Sans.ttf");
         mProjectNameView.setTypeface(sciFont);
         String[][] todoArrays = {{"Make thing work", "Make it not look terrible", "Figure out how to do the thing"}, {"Make it stop doing the thing", "Figure out why its doing the thing", "Make the colors not eyesearing"}, {"Add feature", "Remove feature", "Think of more features"}, {"Refactor", "Rewrite", "Recycle"}};
-        ArrayList<String> thisArray = new ArrayList<>(Arrays.asList(todoArrays[position]));
-        Log.i(TAG, "onCreate: " + thisArray.toString());
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.custom_todo_list_item, thisArray);
-        mTodoListView.setAdapter(adapter);
+        mThisTodoArray = new ArrayList<>(Arrays.asList(todoArrays[position]));
+        Log.i(TAG, "onCreate: " + mThisTodoArray.toString());
+        mAdapter = new ArrayAdapter(this, R.layout.custom_todo_list_item, mThisTodoArray);
+        mTodoListView.setAdapter(mAdapter);
+        mAddTodoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newTodo = mAddTodoInput.getText().toString();
+                if(newTodo.length() > 0){
+                    mThisTodoArray.add(newTodo);
+                    mAdapter.notifyDataSetChanged();
+
+                    //for hiding the keyboard after button is pressed
+                    InputMethodManager imm = (InputMethodManager)getApplication().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(mAddTodoInput.getWindowToken(), 0);
+                } else {
+                    Toast.makeText(TodosActivity.this, "Please enter a todo first!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 }
